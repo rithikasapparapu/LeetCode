173eb211f59aa7666c959e265c120982f7f6c3be
rithikasapparapu <rithikasapparapu@tamu.edu>
2024-10-19 16:35:34 -0500

    Time: 3 ms (72.86%), Space: 43.1 MB (64.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAlphaNumeric(char character){
+        if((character >= '0' && character <= '9') || (character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')){
+            return true;
+        }
+        return false;
+    }
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            while(!isAlphaNumeric(s.charAt(l))){
+                l++;
+                if(l>=s.length()){
+                    return true;
+                }
+            }
+            while(!isAlphaNumeric(s.charAt(r))){
+                r--;
+                if(r<0){
+                    break;
+                }
+            }
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
