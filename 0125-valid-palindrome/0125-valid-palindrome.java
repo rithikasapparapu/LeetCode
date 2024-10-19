6efa0651379d4b81a4980573af1fac44af16c900
rithikasapparapu <rithikasapparapu@tamu.edu>
2024-10-19 16:42:23 -0500

    Time: 16 ms (19.55%), Space: 44.7 MB (35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,24 +6,34 @@ class Solution {
         return false;
     }
     public boolean isPalindrome(String s) {
+        // s = s.toLowerCase();
+        // int l = 0;
+        // int r = s.length()-1;
+        // while(l<r){
+        //     while(!isAlphaNumeric(s.charAt(l))){
+        //         l++;
+        //         if(l>=s.length()){
+        //             return true;
+        //         }
+        //     }
+        //     while(!isAlphaNumeric(s.charAt(r))){
+        //         r--;
+        //     }
+        //     if(s.charAt(l)!=s.charAt(r)){
+        //         return false;
+        //     }
+        //     l++;
+        //     r--;
+        // }
+        // return true;
+        
         s = s.toLowerCase();
-        int l = 0;
-        int r = s.length()-1;
-        while(l<r){
-            while(!isAlphaNumeric(s.charAt(l))){
-                l++;
-                if(l>=s.length()){
-                    return true;
-                }
-            }
-            while(!isAlphaNumeric(s.charAt(r))){
-                r--;
-            }
-            if(s.charAt(l)!=s.charAt(r)){
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+
+        for(int i=0; i<s.length()/2; i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
                 return false;
             }
-            l++;
-            r--;
         }
         return true;
     }
