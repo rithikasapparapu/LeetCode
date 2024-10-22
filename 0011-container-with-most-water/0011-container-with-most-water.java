6d7ac7db6e4f49e20441d20d123fec71da1032f8
rithikasapparapu <rithikasapparapu@tamu.edu>
2024-10-22 09:13:00 -0500

    Time: 5 ms (72.47%), Space: 57.3 MB (78.66%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,7 +4,6 @@ class Solution {
         int r = height.length - 1;
         int volume = 0;
         while(l<r){
-
             if(volume < (r-l) * Math.min(height[r],height[l])){
                 volume = (r-l) * Math.min(height[r],height[l]);
             }
