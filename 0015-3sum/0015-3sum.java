012f8e15a11cde2f345e83dbe6e8fe70b6aef9a5
rithikasapparapu <rithikasapparapu@tamu.edu>
2024-10-22 08:47:27 -0500

    Time: 31 ms (58.46%), Space: 51.2 MB (75.94%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> s = new HashSet<>();
-        List<List<Integer>> result= new ArrayList<>();
-        for(int i=0; i<nums.length-2; i++){
-            int l = i+1;
-            int r = nums.length-1;
-            while(l<r){
-                if(nums[i]+nums[l]+nums[r]==0){
-                    //add to the list
-                    s.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                    l++;
-                    r--;
-                }
-                else if(nums[i]+nums[l]+nums[r]<0){
-                    l++;
-                }
-                else{
-                    r--;
-                }
-            }
-        }
-        result.addAll(s);
-        return result;
-    }
-}
-
-
-
-/*
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result= new ArrayList<>();
         for(int i=0; i<nums.length-2; i++){
             if(i>0 && nums[i]==nums[i-1]){
@@ -58,6 +27,35 @@ class Solution {
                 }
             }
         }
+        return result;
+    }
+}
+
+/*
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> result= new ArrayList<>();
+        for(int i=0; i<nums.length-2; i++){
+            int l = i+1;
+            int r = nums.length-1;
+            while(l<r){
+                if(nums[i]+nums[l]+nums[r]==0){
+                    //add to the list
+                    s.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                }
+                else if(nums[i]+nums[l]+nums[r]<0){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+            }
+        }
+        result.addAll(s);
         return result;
     }
 }
