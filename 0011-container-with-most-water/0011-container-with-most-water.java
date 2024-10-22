d93ccaa9c85456571116d1d18113a02ad2d37637
rithikasapparapu <rithikasapparapu@tamu.edu>
2024-10-22 09:12:44 -0500

    Time: 5 ms (72.47%), Space: 57.9 MB (38.56%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int volume = 0;
+        while(l<r){
+
+            if(volume < (r-l) * Math.min(height[r],height[l])){
+                volume = (r-l) * Math.min(height[r],height[l]);
+            }
+            if(height[l]==height[r]){
+                l++;
+                r--;
+            }
+            else if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return volume;
+    }
+}
