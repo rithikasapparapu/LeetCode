bfecce5ffce693d2a45d1c31b2bf2b72999941ca
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2025-01-03 16:03:28 -0600

    Time: 1 ms (67.38%), Space: 89.6 MB (38.92%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        int result = 0;
-        long long right_sum = 0;
-        long long left_sum = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            right_sum += nums[i];
+        long long sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
         }
+        int result = 0;
+        long long prefix_sum = 0;
         for(int i=0; i<nums.size()-1; i++){
-            if(left_sum >= right_sum){
+            prefix_sum += nums[i];
+            if(prefix_sum >= sum - prefix_sum){
                 result++;
             }
-            left_sum += nums[i+1];
-            right_sum -= nums[i+1];
         }
         return result;
     }
