8806b21d2323bb2379cfa21e6f724acf45b6a071
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2025-01-30 13:30:09 -0600

    Time: 0 ms (100%), Space: 8.6 MB (83.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack <char> st;
-        for(int i=0; i<s.size(); i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-                st.push(s[i]);
-            }
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
             else{
-                if(!st.empty()){
-                    if((st.top()=='(' && s[i]==')')|| (st.top()=='{' && s[i]=='}') || (st.top()=='[' && s[i]==']'))
-                        st.pop();
-                    else
-                        return false;
+                if(st.empty()) return false;
+                if(s[i]==')'){
+                    if(st.top()!='(') return false;
+                    st.pop();
                 }
-                else
-                    return false;
+                else if(s[i]=='}'){
+                    if(st.top()!='{') return false;
+                    st.pop();
+                }
+                else{
+                    if(st.top()!='[') return false;
+                    st.pop();
+                }
             }
         }
-        if(!st.empty())
-            return false;
+        if(!st.empty()) return false;
         return true;
     }
 };
