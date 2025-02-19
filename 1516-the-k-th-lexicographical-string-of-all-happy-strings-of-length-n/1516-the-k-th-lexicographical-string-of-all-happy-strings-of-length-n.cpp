c6d9c58e4d2231e5d74d3a3fdf2126fd1885354e
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2025-02-19 00:46:50 -0600

    Time: 4 ms (63.82%), Space: 11.2 MB (58.45%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool btk(vector<string>& res, string temp, int n, int k, string happy){
+        if(temp.length()==n){
+            res.push_back(temp);
+            if(res.size()==k){
+                return true;
+            }
+            return false;
+        }
+        bool b;
+        for(int i=0; i<happy.length(); i++){
+            if(temp.empty()){
+                temp += happy[i];
+                b = btk(res, temp, n, k, happy);
+                if(b) break;
+                temp.pop_back();
+            }
+            else{
+                if(happy[i]!=temp.back()){
+                    temp += happy[i];
+                    b = btk(res, temp, n, k, happy);
+                    if(b) break;
+                    temp.pop_back();
+                }
+            }
+        }
+        if(b) return true;
+        return false;
+    }
+    string getHappyString(int n, int k) {
+        vector<string> res;
+        string temp = "";
+        string happy = "abc";
+        if (!btk(res, temp, n, k, happy)) return "";
+        else return res.back();
+    }
+};
