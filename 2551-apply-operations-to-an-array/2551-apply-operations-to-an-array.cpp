a23b2ac9a612a982e9fc566cf7dc66bb92779022
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2025-03-01 13:26:53 -0600

    Time: 0 ms (100%), Space: 13 MB (99.53%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -7,14 +7,19 @@ public:
                 nums[i+1] = 0;
             }
         }
-        vector<int> res(nums.size(),0);
-        int j = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=0){
-                res[j] = nums[i];
-                j++;
+        int t = 0, j = 0;
+        while(j < nums.size()) {
+            if(nums[j] != 0) {
+                nums[t] = nums[j];
+                t++;
             }
+            j++;
         }
-        return res;
+        while(t < nums.size()) {
+            nums[t] = 0;
+            t++;
+        }
+        
+        return nums;
     }
 };
