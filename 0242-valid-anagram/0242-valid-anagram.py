435def226cf27ef512f2cb1a51d9da1689add2ff
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2025-07-17 01:29:21 -0500

    Time: 15 ms (41.85%), Space: 18.6 MB (11.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if sorted(s) == sorted(t):
             return True
         return False
-        
-        
